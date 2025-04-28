b4ed6f1395ca2bd454c749beefe0c757e4b05079
HaraldM <harald.magnusson24@gmail.com>
2025-04-28 23:46:00 +0200

    day7: refactor out launch arg parsing and file reading to new function

--- day7/main.zig
+++ day7/main.zig
@@ -1,27 +1,44 @@
 const std = @import("std");
 
 pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    var iter = getFileIteratorFromLaunchArg(&arena) catch |err| switch (err) {
+        error.NoArgsGiven => {
+            std.debug.print("No filename given\n", .{});
+            return err;
+        },
+        else => return err,
+    };
+
+    while (iter.next()) |row| {
+        std.debug.print("{s}\n", .{row});
+    }
+}
+
+/// Returns an interator that iterates through each line the file
+/// specified by the first launch argument.
+/// Uses arena allocator to not need to free file content buffer.
+/// Reads a maximum of 100 MiB from file.
+pub fn getFileIteratorFromLaunchArg(
+    arena: *std.heap.ArenaAllocator,
+) !std.mem.SplitIterator(u8, .scalar) {
     var args = std.process.args();
     defer args.deinit();
 
-    _ = args.skip();
+    _ = args.skip(); // skip launch command
 
     const filename = args.next() orelse {
-        std.debug.print("No filename given\n", .{});
-        return;
+        return error.NoArgsGiven;
     };
 
-    std.debug.print("filename: {s}\n", .{filename});
+    const input_file = try std.fs.cwd().openFile(filename, .{});
+    defer input_file.close();
 
-    const test_file = try std.fs.cwd().openFile(filename, .{});
-
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
     const allocator = arena.allocator();
+    const max_buf_size = 100 * 1024 * 1024;
+    const content = try input_file.readToEndAlloc(allocator, max_buf_size);
 
-    const max_buf_size = 1024 * 1024;
-    const content = try test_file.readToEndAlloc(allocator, max_buf_size);
-
-    std.debug.print("data:\n{s}\n", .{content});
-    // user std.mem.split to get an iterator for file, maybe refactor into a general file reader to use each day/test
+    return std.mem.splitScalar(u8, content, '\n');
 }
