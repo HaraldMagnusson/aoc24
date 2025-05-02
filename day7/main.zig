3486297c9e95093e9bce2e41f3e1fcc3aebdd5e9
HaraldM <harald.magnusson24@gmail.com>
2025-05-02 19:23:29 +0200

    day7: add part 1 implementation

--- day7/main.zig
+++ day7/main.zig
@@ -4,25 +4,35 @@ const extras = @import("extras");
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
-    const allocator = arena.allocator();
+    const alloc = arena.allocator();
 
-    const input_file = extras.readFileFromCmdArg(allocator) catch |err| switch (err) {
+    const puzzle_input = extras.readFileFromCmdArg(alloc) catch |err| switch (err) {
         error.NoArgsGiven => return,
         error.FileNotFound => return,
         else => return err,
     };
-    defer allocator.free(input_file);
-    var iter = std.mem.splitScalar(u8, input_file, '\n');
+    defer alloc.free(puzzle_input);
 
-    while (iter.next()) |row| {
-        std.debug.print("{s}\n", .{row});
-    }
+    std.debug.print(
+        "\nPart 1: sum of valid calibrations: {d}\n\n",
+        .{try calcSumOfValidCalibrations(puzzle_input, alloc)},
+    );
 }
 
-fn testFunc(data: []const u8) !i64 {
-    _ = data;
-    return 3749;
-    //return 42;
+fn calcSumOfValidCalibrations(data: []const u8, alloc: std.mem.Allocator) !u64 {
+    var iter = std.mem.splitScalar(u8, data, '\n');
+
+    var valid_result_sum: u64 = 0;
+    while (iter.next()) |row| {
+        if (row.len == 0) continue;
+
+        const cal = try Calibration.parseFromStr(row, alloc);
+        defer cal.deinit();
+        if (cal.isValid()) {
+            valid_result_sum += cal.result;
+        }
+    }
+    return valid_result_sum;
 }
 
 test "day7 aoc input" {
@@ -31,27 +41,28 @@ test "day7 aoc input" {
 
     try extras.runAocTest(
         "day7/test1.input",
-        i64,
-        &testFunc,
+        u64,
+        &calcSumOfValidCalibrations,
         arena.allocator(),
     );
 }
 
 const Operator = enum { add, mult };
-const operators: []Operator = .{ .add, .mult };
+const operators = [_]Operator{ .add, .mult };
 
 const Calibration = struct {
     result: u64,
     values: []const u64,
+    alloc: std.mem.Allocator,
 
-    fn parseFromStr(str: []const u8, allocator: std.mem.Allocator) !Calibration {
+    fn parseFromStr(str: []const u8, alloc: std.mem.Allocator) !Calibration {
         var str_iter = std.mem.splitAny(u8, str, ": ");
 
         const cal_result_str = str_iter.first();
         const cal_result = try std.fmt.parseInt(u64, cal_result_str, 10);
         _ = str_iter.next(); // skip empty entry coming from ": "
 
-        var result_list = std.ArrayList(u64).init(allocator);
+        var result_list = std.ArrayList(u64).init(alloc);
 
         while (str_iter.next()) |item_str| {
             const item_int = try std.fmt.parseInt(u64, item_str, 10);
@@ -61,7 +72,48 @@ const Calibration = struct {
         return Calibration{
             .result = cal_result,
             .values = try result_list.toOwnedSlice(),
+            .alloc = alloc,
         };
+    }
+
+    fn deinit(self: Calibration) void {
+        self.alloc.free(self.values);
+    }
+
+    fn isValid(self: Calibration) bool {
+        const start_value = self.values[0];
+
+        var is_valid = false;
+        for (operators) |operator| {
+            is_valid = is_valid or self.isValidRecursiveCalc(operator, 1, start_value);
+        }
+        return is_valid;
+    }
+
+    fn isValidRecursiveCalc(
+        self: Calibration,
+        op: Operator,
+        depth: usize,
+        cumulative_result: u64,
+    ) bool {
+        if (depth == self.values.len) {
+            return cumulative_result == self.result;
+        }
+
+        const current_result = switch (op) {
+            .add => cumulative_result + self.values[depth],
+            .mult => cumulative_result * self.values[depth],
+        };
+
+        var is_valid = false;
+        for (operators) |operator| {
+            is_valid = is_valid or self.isValidRecursiveCalc(
+                operator,
+                depth + 1,
+                current_result,
+            );
+        }
+        return is_valid;
     }
 };
 
@@ -72,4 +124,6 @@ test Calibration {
 
     try std.testing.expectEqual(3420, cal.result);
     try std.testing.expectEqualSlices(u64, &[_]u64{ 81, 42, 13 }, cal.values);
+
+    try std.testing.expectEqual(false, cal.isValid());
 }
--- extras.zig
+++ extras.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
 
 /// reads and returns a maximum of 100 MiB from the file given by the first cmd line argument
-pub fn readFileFromCmdArg(allocator: std.mem.Allocator) ![]const u8 {
-    var args = try std.process.argsWithAllocator(allocator);
+pub fn readFileFromCmdArg(alloc: std.mem.Allocator) ![]const u8 {
+    var args = try std.process.argsWithAllocator(alloc);
     defer args.deinit();
 
     _ = args.skip(); // skip launch command
@@ -22,27 +22,27 @@ pub fn readFileFromCmdArg(allocator: std.mem.Allocator) ![]const u8 {
     defer input_file.close();
 
     const max_buf_size = 100 * 1024 * 1024;
-    return try input_file.readToEndAlloc(allocator, max_buf_size);
+    return try input_file.readToEndAlloc(alloc, max_buf_size);
 }
 
 pub fn runAocTest(
     data_filename: []const u8,
     comptime IntType: type,
-    testFunc: *const fn ([]const u8) anyerror!IntType,
-    allocator: std.mem.Allocator,
+    testFunc: *const fn ([]const u8, std.mem.Allocator) anyerror!IntType,
+    alloc: std.mem.Allocator,
 ) !void {
     comptime std.debug.assert(@typeInfo(IntType) == .int);
 
     const data_file = try std.fs.cwd().openFile(data_filename, .{});
     defer data_file.close();
 
-    const data = try data_file.readToEndAlloc(allocator, 100 * 1024 * 1024);
-    defer allocator.free(data);
+    const data = try data_file.readToEndAlloc(alloc, 100 * 1024 * 1024);
+    defer alloc.free(data);
 
     var iter = std.mem.splitScalar(u8, data, '\n');
 
     const result_str = iter.next() orelse return error.EmptyFile;
     const result = try std.fmt.parseInt(IntType, result_str, 10);
 
-    try std.testing.expectEqual(result, testFunc(iter.rest()));
+    try std.testing.expectEqual(result, testFunc(iter.rest(), alloc));
 }
