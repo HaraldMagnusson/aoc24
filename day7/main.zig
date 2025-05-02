06a351da05ca73e9f7a58080c242191a8b3b0f78
HaraldM <harald.magnusson24@gmail.com>
2025-05-02 17:31:08 +0200

    day7: add Calibration type with parsing

--- day7/main.zig
+++ day7/main.zig
@@ -21,8 +21,8 @@ pub fn main() !void {
 
 fn testFunc(data: []const u8) !i64 {
     _ = data;
-    //return 3749;
-    return 42;
+    return 3749;
+    //return 42;
 }
 
 test "day7 aoc input" {
@@ -35,4 +35,41 @@ test "day7 aoc input" {
         &testFunc,
         arena.allocator(),
     );
+}
+
+const Operator = enum { add, mult };
+const operators: []Operator = .{ .add, .mult };
+
+const Calibration = struct {
+    result: u64,
+    values: []const u64,
+
+    fn parseFromStr(str: []const u8, allocator: std.mem.Allocator) !Calibration {
+        var str_iter = std.mem.splitAny(u8, str, ": ");
+
+        const cal_result_str = str_iter.first();
+        const cal_result = try std.fmt.parseInt(u64, cal_result_str, 10);
+        _ = str_iter.next(); // skip empty entry coming from ": "
+
+        var result_list = std.ArrayList(u64).init(allocator);
+
+        while (str_iter.next()) |item_str| {
+            const item_int = try std.fmt.parseInt(u64, item_str, 10);
+            try result_list.append(item_int);
+        }
+
+        return Calibration{
+            .result = cal_result,
+            .values = try result_list.toOwnedSlice(),
+        };
+    }
+};
+
+test Calibration {
+    const input = "3420: 81 42 13";
+    const cal = try Calibration.parseFromStr(input, std.testing.allocator);
+    defer std.testing.allocator.free(cal.values);
+
+    try std.testing.expectEqual(3420, cal.result);
+    try std.testing.expectEqualSlices(u64, &[_]u64{ 81, 42, 13 }, cal.values);
 }
