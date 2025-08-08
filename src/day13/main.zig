be6de3105cf6458d11aec7b1afc50f07410ce1c3
Harald Magnusson <harald.magnusson24@gmail.com>
2025-08-08 21:06:58 +0200

    day13: part 2

--- src/day13/main.zig
+++ src/day13/main.zig
@@ -5,7 +5,7 @@ test "first example" {
     const ally = std.testing.allocator;
     const test_path = "src/day13/test1.in";
 
-    try extras.runAocTest(test_path, u32, totalTokenCost, ally);
+    try extras.runAocTest(test_path, i64, totalTokenCost, ally);
 }
 
 pub fn main() !void {
@@ -22,27 +22,27 @@ pub fn main() !void {
     );
 }
 
-fn totalTokenCost(definition: []const u8, ally: std.mem.Allocator) !u32 {
+fn totalTokenCost(definition: []const u8, ally: std.mem.Allocator) !i64 {
     _ = ally;
 
     var machine_iter = std.mem.splitSequence(u8, definition, "\n\n");
 
-    var total_cost: u32 = 0;
+    var total_cost: i64 = 0;
 
     while (machine_iter.next()) |def| {
-        const machine_def = MachineDef.parse(def);
-        total_cost += machine_def.tokensRequiredForSolution() orelse 0;
+        const machine = Machine.parse(def);
+        total_cost += machine.tokensRequiredForSolution() orelse 0;
     }
 
     return total_cost;
 }
 
-const MachineDef = struct {
+const Machine = struct {
     a: @Vector(2, i64),
     b: @Vector(2, i64),
     price: @Vector(2, i64),
 
-    fn parse(def: []const u8) MachineDef {
+    fn parse(def: []const u8) Machine {
         // format example:
         // Button A: X+94, Y+34
         // Button B: X+22, Y+67
@@ -65,62 +65,37 @@ const MachineDef = struct {
         b[1] = std.fmt.parseUnsigned(i64, iter.next().?, base10) catch unreachable;
 
         _ = iter.next(); // "Price: X"
-        price[0] = std.fmt.parseUnsigned(u32, iter.next().?, base10) catch unreachable;
+        price[0] = std.fmt.parseUnsigned(i64, iter.next().?, base10) catch unreachable;
         _ = iter.next(); // " Y"
-        price[1] = std.fmt.parseUnsigned(u32, iter.next().?, base10) catch unreachable;
+        price[1] = std.fmt.parseUnsigned(i64, iter.next().?, base10) catch unreachable;
 
-        return MachineDef{ .a = a, .b = b, .price = price };
+        const correction: @Vector(2, i64) = @splat(10000000000000);
+        return Machine{ .a = a, .b = b, .price = price + correction };
     }
 
     // returns null if no solution is possible
-    fn tokensRequiredForSolution(self: MachineDef) ?u32 {
-        // simple math solution
-        // https://www.desmos.com/calculator/bqmjdsuqx0
+    fn tokensRequiredForSolution(self: Machine) ?i64 {
+        // linear system of equations: [a b]x = price
 
-        const numerator: i64 = self.b[1] * self.price[0] - self.b[0] * self.price[1];
-        const denominator: i64 = self.b[1] * self.a[0] - self.b[0] * self.a[1];
-
-        // denom == 0 => A and B cause parallel movement of the claw
-        // can A or B be a solution?
-        if (denominator == 0) {
-            const a_count: ?u32 = if (@reduce(
-                .And,
-                @mod(self.price, self.a) == @Vector(2, i64){ 0, 0 },
-            ))
-                @intCast(@divExact(self.price[0], self.a[0]))
-            else
-                null;
-
-            const b_count: ?u32 = if (@reduce(
-                .And,
-                @mod(self.price, self.b) == @Vector(2, i64){ 0, 0 },
-            ))
-                @intCast(@divExact(self.price[0], self.b[0]))
-            else
-                null;
-
-            if (a_count == null and b_count == null) {
-                return null; // no solution
-            }
-
-            // return cheapest
-            const more_than_max_count = 4711;
-            return @min(3 * (a_count orelse more_than_max_count), b_count orelse more_than_max_count);
-        }
-
-        // amount of times to mash A cannot be negative
-        if (std.math.sign(numerator) != std.math.sign(denominator)) {
+        const determinant = self.a[0] * self.b[1] - self.a[1] * self.b[0];
+        if (determinant == 0) {
+            // no unique solution, A and B cause parallel movement
             return null;
         }
 
-        const a_count: u32 = @intCast(std.math.divExact(i64, numerator, denominator) catch return null);
-        const b_count: u32 = @intCast(std.math.divExact(
-            i64,
-            self.price[1] - self.a[1] * a_count,
-            self.b[1],
-        ) catch return null);
+        // Cramers rule
+        const determinant_a = self.price[0] * self.b[1] - self.price[1] * self.b[0];
+        if (@mod(determinant_a, determinant) != 0) {
+            return null;
+        }
+        const a_count = @divExact(determinant_a, determinant);
 
-        const cost: u32 = 3 * @as(u32, @intCast(a_count)) + @as(u32, @intCast(b_count));
-        return cost;
+        const determinant_b = self.a[0] * self.price[1] - self.a[1] * self.price[0];
+        if (@mod(determinant_b, determinant) != 0) {
+            return null;
+        }
+        const b_count = @divExact(determinant_b, determinant);
+
+        return 3 * a_count + b_count;
     }
 };
